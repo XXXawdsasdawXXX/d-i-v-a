8f25a9ef29da247561d7c6b2d78a0c0b2eba37ca
6awdsasdaw9 <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2023-12-09 17:22:15 +0300

    ButtonSprite

---
+++ Assets/Code/Components/ButtonSprite.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Test
+{
+    public class ButtonSprite : MonoBehaviour
+    {
+        [SerializeField] private bool _isPressed;
+
+        private void Update()
+        {
+            if (_isPressed)
+            {
+                var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                pos.z = 0;
+                transform.position = pos;
+            }
+        }
+
+        void OnMouseDown()
+        {
+            _isPressed = true;
+            Debug.Log("Mouse down");
+        }
+
+        private void OnMouseUp()
+        {
+            _isPressed = false;
+            Debug.Log("Mouse up");
+        }
+
+        private void OnMouseEnter()
+        {
+            Debug.Log("Mouse enter");
+        }
+
+        private void OnMouseExit()
+        {
+            Debug.Log("Mouse exit");
+        }
+
+        private void OnMouseOver()
+        {
+            Debug.Log("Mouse over");
+        }
+    }
+}
